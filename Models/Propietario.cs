967a0cddb7b1d5c51523cb45e70df52c15da4a2a
Juan Becerra <juan_matias_becerra@hotmail.com>
2024-08-18 03:59:29 -0300

    Crear clases propietario e inquilino

---
+++ Models/Propietario.cs
@@ -0,0 +1,9 @@
+public class Propietario{
+    private int id_propietario {get; set;}
+    private string nombre {get;set;}
+    private string apellido {get; set;}
+    private string direccion {get; set;}
+    private string telefono {get; set;}
+    
+    
+}
---
+++ Models/inquilino.cs
@@ -0,0 +1,9 @@
+public class Inquilino
+{
+    private int id_inquilino {get; set;}
+    private string nombre {get; set;}
+    private string apellido {get; set;}
+    private string direccion {get; set;}
+    private string telefono {get; set;}
+    
+}
