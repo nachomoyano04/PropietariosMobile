7896904aae68159585386ea8b892fd2c0a5467ae
juan-becerra-netbook <juan_matias_becerra@hotmail.com>
2024-08-23 20:34:36 -0300

    Metodos corregidos de RepositorioInquilinos y RepositorioPopietarios

--- Models/RepositorioPropietario.cs
+++ Models/RepositorioPropietario.cs
@@ -6,130 +6,41 @@ using MySql.Data.MySqlClient;
 namespace ProyetoInmobiliaria.Models;
 
 public class RepositorioPropietario{
-    string ConnectionString = "Server=localhost;User=root;Password=;Database=inmobiliaria;SslMode=none";
+    
     
     private readonly InmobiliariaContext _context = new InmobiliariaContext();
 
     public List<Propietario> Listar(){
-        // List<Propietario> propietarios = new List<Propietario>();
-        // using(MySqlConnection connection = new MySqlConnection(ConnectionString)){
-        //     var query = "SELECT * FROM propietario";
-        //     using(MySqlCommand command = new MySqlCommand(query, connection)){
-        //         connection.Open();
-        //         var reader = command.ExecuteReader();
-        //         while(reader.Read()){
-        //             propietarios.Add(new Propietario{
-        //                 IdPropietario = reader.GetInt32("idPropietario"),
-        //                 Apellido= reader.GetString("Apellido"),
-        //                 Nombre = reader.GetString("Nombre"),
-        //                 Dni = reader.GetString("dni"),
-        //                 Telefono = reader.GetString("Telefono"),
-        //                 Correo = reader.GetString("correo")
-        //             });
-        //         }
-        //         connection.Close();
-        //     }
-        //     return propietarios;
-        // }
+        
         return _context.Propietario.ToList();
     }
     public Propietario? Obtener(int Id){
-        Propietario? propietario = null;
-        using(MySqlConnection connection = new MySqlConnection(ConnectionString)){
-            var query = $@"SELECT * FROM propietario WHERE idPropietario = @IdPropietario";
-            using(MySqlCommand command = new MySqlCommand(query, connection)){
-                command.Parameters.AddWithValue("@IdPropietario", Id);
-                connection.Open();
-                var reader = command.ExecuteReader();
-                if(reader.Read()){
-                    propietario = new Propietario{
-                        IdPropietario = reader.GetInt32("IdPropietario"),
-                        Apellido= reader.GetString("Apellido"),
-                        Nombre = reader.GetString("Nombre"),
-                        Dni = reader.GetString("Dni"),
-                        Telefono = reader.GetString("Telefono"),
-                        Correo = reader.GetString("Correo")
-                    };
-                }
-                connection.Close();
-            }
+        Propietario? propietario = _context.Propietario.Find(Id);
             return propietario;
         }
-    }
-    public int Crear(Propietario propietario){
-        // int respuesta = -1;
-        // using(MySqlConnection connection = new MySqlConnection(ConnectionString)){
-        //     var query = $@"INSERT INTO propietario (dni, apellido, nombre, telefono, correo, estado) VALUES (@Dni, @Apellido, @Nombre, @Telefono, @Correo, 1);SELECT LAST_INSERT_ID();";
-        //     using(MySqlCommand command = new MySqlCommand(query, connection)){
-        //         command.Parameters.AddWithValue("@Dni", propietario.Dni);
-        //         command.Parameters.AddWithValue("@Apellido", propietario.Apellido);
-        //         command.Parameters.AddWithValue("@Nombre", propietario.Nombre);
-        //         command.Parameters.AddWithValue("@Telefono", propietario.Telefono);
-        //         command.Parameters.AddWithValue("@Correo", propietario.Correo);
-        //         connection.Open();
-        //         respuesta = Convert.ToInt32(command.ExecuteScalar());
-        //         connection.Close();
-        //     }
-        // }
 
+    
+    public int Crear(Propietario propietario){
+        
         _context.Propietario.Add(propietario);
         _context.SaveChanges();
         return propietario.IdPropietario;
     }
 
-
-    //  public int crearPropietario(Propietario propietario){
-    //     _context.Propietarios.add(propietario);
-
-    //     _context.SaveChanges();
-    //     return propietario.getId();
-    // }
-
-    // public Propietario ObtenerInquilino(int id){
-    //     return _context.Propietarios.Find(id);
-    // }
-
-    // public void ActualizarInquilino(Propietario propietario){
-    //     _context.Propietarios.Update(propietario);
-    //     _context.SaveChanges();
-    // }
-
-    // public void darDeBaja(int id){
-    //     Propietario i = _context.Propietarios.Find(id);
-    //     _context.Propietario.Remove(i);
-    //     _context.SaveChanges();
-    // }
-    // public List<Propietario> obtenerPropietarios(){
-    //     return _context.Propietarios.ToList();}
-
-
     public int Modificar(Propietario propietario){
-        int respuesta = -1;
-        // using(MySqlConnection connection = new MySqlConnection(ConnectionString)){
-        //     var query = $@"UPDATE propietario SET dni = @Dni, apellido = @Apellido, nombre = @Nombre, telefono = @Telefono, correo = @Correo WHERE idPropietario = @IdPropietario";
-        //     using(MySqlCommand command = new MySqlCommand(query, connection)){
-        //         command.Parameters.AddWithValue("@Dni", propietario.Dni);
-        //         command.Parameters.AddWithValue("@Apellido", propietario.Apellido);
-        //         command.Parameters.AddWithValue("@Nombre", propietario.Nombre);
-        //         command.Parameters.AddWithValue("@Telefono", propietario.Telefono);
-        //         command.Parameters.AddWithValue("@Correo", propietario.Correo);
-        //         command.Parameters.AddWithValue("@IdPropietario", propietario.IdPropietario);
-        //         connection.Open();
-        //         respuesta = command.ExecuteNonQuery();
-        //         connection.Close();
-        //     }
-        // }
+        
+        
         _context.Propietario.Update(propietario);
         _context.SaveChanges();
-        return respuesta;
+        return 1;
 
     }
 
     public void Eliminar(int Id){
-        Propietario propietario = _context.Propietario.Find(Id); // Debería ser Inquilino
+        Propietario propietario = _context.Propietario.Find(Id); 
         if (propietario != null){
             propietario.Estado = false;
-            _context.Propietario.Update(propietario); // Debería ser Inquilino
+            _context.Propietario.Update(propietario); 
             _context.SaveChanges();
         }
     }
