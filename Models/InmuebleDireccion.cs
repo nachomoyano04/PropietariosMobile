9a8bf426ea5527924444774b54b518858b23f0a3
Juan Becerra <juan_matias_becerra@hotmail.com>
2024-09-19 16:51:19 -0300

    cambios en atributos de InmuebleDireccion

--- Models/InmuebleDireccion.cs
+++ Models/InmuebleDireccion.cs
@@ -1,11 +1,11 @@
 using ProyetoInmobiliaria.Models;
 
 public class InmuebleDireccion{
-    public string Inmueble {get; set;}
-    public string Direccion {get; set;}
-    public override string ToString(){
+    public Inmueble Inmueble {get; set;}
+    public Direccion Direccion {get; set;}
+    /*public override string ToString(){
         return Direccion;
-    }
+    }*/
 
 
 }
