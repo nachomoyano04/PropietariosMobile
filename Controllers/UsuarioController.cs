5ad0e277063249a0d9c69ef8970ea0fc35ecc362
Juan Becerra <juan_matias_becerra@hotmail.com>
2024-09-20 15:54:19 -0300

    Agregar Avatar

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -32,11 +32,40 @@ public class UsuarioController: Controller{
         return View();
     }
     [AllowAnonymous]
-    public IActionResult Guardar(Usuario usuario){
-        
+    public IActionResult Guardar(Usuario usuario, IFormFile archivo){
+        usuario.Avatar= "/img/Avatar/default.jpg";
+        if(archivo!=null){
+            var fileName= Path.GetFileName(archivo.FileName);
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/img/Avatar",fileName);
+            using (var fileStream = new FileStream(filePath,FileMode.Create))
+            {
+                archivo.CopyTo(fileStream);
+            }
+            usuario.Avatar = "/img/Avatar/"+fileName;
+        }
+
+
         RepositorioUsuario repositorio = new RepositorioUsuario();
         repositorio.Guardar(usuario);
         return RedirectToAction( "Index", "Login");
     }
+    /*[AllowAnonymous]
+public IActionResult Guardar(Usuario usuario, IFormFile archivo)
+{
+    RepositorioUsuario repositorio = new RepositorioUsuario();
+    if (archivo != null)
+    {
+        var fileName = Path.GetFileName(archivo.FileName);
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/imagenes", fileName);
+        using (var fileStream = new FileStream(filePath, FileMode.Create))
+        {
+            archivo.CopyTo(fileStream);
+        }
+        usuario.Imagen = "/imagenes/" + fileName;
+    }
+    repositorio.Guardar(usuario);
+    return RedirectToAction("Index", "Login");
+}
+```*/
     
 }
